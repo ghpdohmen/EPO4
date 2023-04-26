a8732d2575dbbd9139c156cb147d11508a42322a
Djordi <idqkruyt@gmail.com>
2023-04-26 12:04:12 +0200

    Merge to main

--- Code/subsystems/microphone_array.py
+++ Code/subsystems/microphone_array.py
@@ -1,1 +1,17 @@
+<<<<<<< Updated upstream
 stream = pyaudio_handle.open(input_device_index=device_index, channels=5, format=pyaudio.paInt16, rate=Fs, input=True)
+=======
+import pyaudio as audio
+
+pyaudio_handle = audio.PyAudio()
+
+for i in range(pyaudio_handle.get_device_count()):
+    device_info = pyaudio_handle.get_device_info_by_index(i)
+    print(i, device_info['name'])
+
+
+
+Fs = 44100
+# stream = pyaudio_handle.open(input_device_index= soundcard_naam, channels=5, format=pyaudio.paInt16, rate=Fs, input=True)
+
+>>>>>>> Stashed changes
