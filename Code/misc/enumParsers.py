34b93bd436ac1ead1338c61db96444b38effc55c
Guus Dohmen <guusdohmen@hotmail.nl>
2023-04-28 14:47:36 +0200

    iets

---
+++ Code/misc/enumParsers.py
@@ -0,0 +1,25 @@
+from misc.robotModeEnum import robotMode
+
+
+def robotModeParser(i):
+    """
+    Parses an int to a robotmode
+    @param i: int index
+    @return: robotmode
+    """
+    if i == 0:
+        return robotMode.Manual
+    elif i == 1:
+        return robotMode.ChallengeA
+    elif i == 2:
+        return robotMode.ChallengeB
+    elif i == 3:
+        return robotMode.ChallengeC
+    elif i == 4:
+        return robotMode.ChallengeD
+    elif i == 5:
+        return robotMode.ChallengeE
+    elif i == 7:
+        return robotMode.EStop
+    else:
+        return robotMode.NotChosen
