938d6a321a41ca55907953df2a65ae86682b65b0
albypearl <alby.vdv1@gmail.com>
2023-04-28 17:15:47 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -52,7 +52,12 @@ a = App()
 
 root = Tk()
 root.title("GUI for EPO4")
-window = ttk.Frame(root)
+
+# little window to show data
+window = ttk.Frame(root, width=200, height=100)
+window["borderwidth"] = 4
+window["relief"] = "ridge"
+# end window
 
 # graph showing path
 figure = plt.Figure(figsize=(2, 2), dpi=100)
@@ -60,8 +65,6 @@ axis = figure.add_subplot(111)
 axis.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
 canvas = FigureCanvasTkAgg(figure)
 canvas.draw()
-
-
 # end graph
 
 # robot declaration
@@ -81,12 +84,34 @@ lb_programs = Listbox(root, height=5, listvariable=l_programsvar)
 lb_programs.bind("<Double-Button-1>", lambda event: program_selector())
 # end listbox
 
+# labels
+label_1 = ttk.Label(window, text="speed: ")
+label_m = ttk.Label(window)
+motorSpeedText = StringVar()
+label_m["textvariable"] = motorSpeedText
+motorSpeedText.set(robot.Robot.input_motor)
+
+label_2 = ttk.Label(window, text="battery: ")
+label_b = ttk.Label(window)
+batteryText = StringVar()
+label_b["textvariable"] = batteryText
+batteryText.set(robot.Robot.batteryVoltage)
+
+label_3 = ttk.Label(window, text="direction: ")
+label_d = ttk.Label(window)
+directionText = StringVar()
+label_b["textvariable"] = directionText
+batteryText.set(robot.Robot.input_servo)
+#end labels
+
 # buttons
 # start button
 button_start = ttk.Button(root, text="START", command=a.startRobot)
 
+# stop button
 button_stop = ttk.Button(root, text="DISABLE ROBOT", command=a.stopRobot)
 
+# emergency motor stop
 button_estop = ttk.Button(root, text="STOP MOTORS", command=a.estop)
 
 # bindings
@@ -97,14 +122,16 @@ root.bind("<s>", lambda event: a.m_backward())
 root.bind("<d>", lambda event: a.m_right())
 # end buttons
 
-
-
-# speed bar
-speed = ttk.Progressbar(root, orient=HORIZONTAL, length=200, maximum=30, mode="determinate")
-
-# end speed bar
-
 # grid for everything
+window.grid(column=0, row=4)
+label_1.grid(column=0, row=0)
+label_m.grid(column=1, row=0)
+label_2.grid(column=0, row=1)
+label_b.grid(column=1, row=1)
+label_3.grid(column=0, row=2)
+label_d.grid(column=1, row=2)
+
+
 canvas.get_tk_widget().grid(column=0, row=0, columnspan=3, rowspan=3)
 
 button_start.grid(column=3, row=0)
@@ -117,8 +144,10 @@ lb_programs.grid(column=3, row=4)
 speed.grid(column=0, columnspan=3)
 # end grid
 
+root.mainloop()
+
 while True:
     a.updateRobot()
     root.update()
 
-root.mainloop()
+# root.mainloop()
